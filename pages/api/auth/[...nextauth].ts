801a9940d37cb4cd29d25c3f6f18f176ac841659
terrierscript <13282103+terrierscript@users.noreply.github.com>
2020-08-12 17:49:29 +0900

    add

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -12,14 +12,14 @@ const options = {
   ],
 
   // A database is optional, but required to persist accounts in a database
-  database: process.env.DATABASE_URL,
+  // database: process.env.DATABASE_URL,
   session: {
     jwt: true,
     maxAge: 30 * 24 * 60 * 60, // 30 days
     updateAge: 24 * 60 * 60, // 24 hours
   },
   sessionToken: {
-    name: `__Secure-next-auth.session-token`,
+    name: `__Secure-next-auth.session-token`, 
     options: {
       httpOnly: true,
       sameSite: 'lax',
